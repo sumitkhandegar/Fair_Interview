f31b08930846c94f6952d25c48f52229d9957995
KushGoyner <kushgoynerkg@gmail.com>
2024-09-11 23:32:14 +0530

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Login from './pages/auth/Login';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
